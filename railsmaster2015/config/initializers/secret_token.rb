4b2cb5ed53e40513d82928626cd5df6b369e19dc
harik <hamouda.form@gmail.com>
2015-04-16 18:46:26 +0000

    creation d'une app vide

---
+++ railsmaster2015/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsmaster2015::Application.config.secret_key_base = '7d9375a9c539bfa9438ac8a9ba69998d4ba98663d5a54258c3da7174fb517b56b4e73106b5d64266199b429dae584d04131bdefb5d039b5dbd6848ce43cb79ed'
